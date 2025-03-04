5ebdd4eb32c13e83db3aa6499fe369fef7821c53
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-03-05 00:31:09 +1000

    Adding code in to make tests pass. Just one more!

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -4,7 +4,7 @@ from todo.models.todo import Todo
 from datetime import datetime
  
 api = Blueprint('api', __name__, url_prefix='/api/v1') 
-
+EXPECTED_KEYS = {'id', 'title', 'description', 'completed', 'deadline_at', 'created_at', 'updated_at'}
 TEST_ITEM = {
     "id": 1,
     "title": "Watch CSSE6400 Lecture",
@@ -24,7 +24,21 @@ def health():
 @api.route('/todos', methods=['GET'])
 def get_todos():
     """Return the list of todo items"""
-    todos = Todo.query.all()
+    complete = request.args.get('completed', type=bool)
+    deadline = request.args.get('deadline_at', type=int)
+    today = datetime.now()
+    if complete is not None:
+        todos = Todo.query.filter_by(completed=complete).all() 
+    else:     
+        todos = Todo.query.all()
+    """
+    if deadline is not None: 
+        todos = Todo.query.filter(Todo.deadline_at > today + timedelta(days=deadline))
+    else:     
+        todos = Todo.query.all()
+    """
+
+
     result = []
     for todo in todos: 
         result.append(todo.to_dict())
@@ -41,6 +55,16 @@ def get_todo(todo_id):
 @api.route('/todos', methods=['POST'])
 def create_todo():
     """Create a new todo item and return the created item"""
+    if not request.is_json: 
+        return jsonify({"error": "Request must be JSON"}), 400
+
+    incoming_keys = set(request.json.keys())
+    if not incoming_keys.issubset(EXPECTED_KEYS):
+        return jsonify({"error": "Unexpected keys in request"}), 400
+    data = request.json
+    if 'title' not in data: 
+        return jsonify({"error": "Missing Title"}), 400
+        
     todo = Todo(
             title=request.json.get('title'),
             description=request.json.get('description'),
@@ -51,7 +75,7 @@ def create_todo():
 
     # Adds a new record to the database or will update an existing record.
     db.session.add(todo)
-    
+
     # Commits the changes to the database.
     # This must be called for the changes to be saved.
     db.session.commit()
@@ -60,16 +84,29 @@ def create_todo():
 @api.route('/todos/<int:todo_id>', methods=['PUT'])
 def update_todo(todo_id):
     """Update a todo item and return the updated item"""
+    if not request.is_json: 
+        return jsonify({"error": "Request must be JSON"}), 400
+    incoming_keys = set(request.json.keys())
+    
+    if not incoming_keys.issubset(EXPECTED_KEYS):
+        return jsonify({"error": "Unexpected keys in request"}), 400
+
+    # todo_id the one that we want to modify.
+    data = request.json
+    if 'id' in data: 
+        return jsonify({"error": "Must not modify private key"}), 400
+
+    
+
     todo = Todo.query.get(todo_id)
     if todo is None:
         return jsonify({'error': 'Todo not found'}), 404
-
     todo.title = request.json.get('title', todo.title)
     todo.description = request.json.get('description', todo.description)
     todo.completed = request.json.get('completed', todo.completed)
     todo.deadline_at = request.json.get('deadline_at', todo.deadline_at)
     db.session.commit()
-    return jsonify(todo.to_dict())
+    return jsonify(todo.to_dict()), 200 
 
 @api.route('/todos/<int:todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
