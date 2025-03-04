562d025fddb3b288a6afeb3ba33dd5fa9e62a1c9
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-03-04 20:53:38 +1000

    Pasted the init code for todo. Added to todo.py for the database. Added author name in project.toml.

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+    """Always check which python we're executing.
+    Make sure its not pointing to conda or Homebrew
+    But a new virtual (poetry) environment that I've made
+    """
 def create_app():
     app = Flask(__name__)
+    # URI -> Uniform Resource Identifier
+    # In production, this URI will define the credentials and 
+    # Hostname of database server.
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.splite"
+    # Load the models
+    from todo.models import db
+    from todo.models.todo import Todo
+    db.init_app(app)
 
-    from .views.routes import api
+    # Create data base variables
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+    
+    # Register the blueprint
+    from todo.views.routes import api
     app.register_blueprint(api)
 
     return app
